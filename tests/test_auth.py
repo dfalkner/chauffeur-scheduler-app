2ebf1bd4dc88459797a63ae39f3bfefd166964df
Dane Falkner <dfalkner@surgeworks.com>
2023-11-14 22:49:04 -0700

    aider: Added test cases for the login route in the `test_auth.py` file.
    # Aider chat conversation:
    
    USER: lets write some tests
    
    ASSISTANT: To write tests for the Chauffeur Scheduler App, we'll need to create a new test file and set up a testing environment. We'll use the `unittest` framework that comes with Python's standard library and Flask's test client to simulate requests to the application.
    
    Here's what we need to do:
    
    1. Create a new test file, for example, `test_auth.py` in a `tests` directory.
    2. Set up the test environment and test cases within this file.
    3. Write tests for the `login` route to ensure it behaves correctly for both GET and POST requests.
    
    Let's start by creating the test file and setting up the test environment.
    
    ```python
    tests/test_auth.py
    <<<<<<< SEARCH
    
    =======
    import unittest
    from flask import Flask, template_rendered
    from contextlib import contextmanager
    from modules.auth.views import auth_blueprint
    
    app = Flask(__name__)
    app.register_blueprint(auth_blueprint, url_prefix='/auth')
    
    @contextmanager
    def captured_templates(app):
        recorded = []
    
        def record(sender, template, context, **extra):
            recorded.append((template, context))
    
        template_rendered.connect(record, app)
        try:
            yield recorded
        finally:
            template_rendered.disconnect(record, app)
    
    class AuthTestCase(unittest.TestCase):
    
        def setUp(self):
            self.app = app.test_client()
            self.app.testing = True
    
        def test_login_get(self):
            with captured_templates(app) as templates:
                response = self.app.get('/auth/login')
                self.assertEqual(response.status_code, 200)
                self.assertEqual(len(templates), 1)
                template, context = templates[0]
                self.assertEqual(template.name, 'login.html')
    
        def test_login_post(self):
            # This test will need to be expanded with logic to mock AWS Cognito authentication
            response = self.app.post('/auth/login', data=dict(
                username='testuser',
                password='testpass'
            ))
            # Check for redirection (302) to the owner dashboard
            self.assertEqual(response.status_code, 302)
            self.assertTrue('/owner/dashboard' in response.headers['Location'])
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> REPLACE
    ```
    
    This test file sets up a basic testing environment and includes two test cases: one for a GET request to the login route, which should render the `login.html` template, and one for a POST request, which should redirect the user to the owner dashboard upon successful login.
    
    Please note that the POST request test is simplified and assumes that the authentication with AWS Cognito is successful. In a real-world scenario, you would need to mock the AWS Cognito authentication process to test the login functionality properly.
    
    If you want to proceed with these changes, please create a `tests` directory and add the `test_auth.py` file with the provided content. If you need assistance with mocking AWS Cognito or writing more comprehensive tests, let me know, and I can provide further guidance.

---
+++ tests/test_auth.py
@@ -0,0 +1,47 @@
+import unittest
+from flask import Flask, template_rendered
+from contextlib import contextmanager
+from modules.auth.views import auth_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+@contextmanager
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+class AuthTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_login_get(self):
+        with captured_templates(app) as templates:
+            response = self.app.get('/auth/login')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(templates), 1)
+            template, context = templates[0]
+            self.assertEqual(template.name, 'login.html')
+
+    def test_login_post(self):
+        # This test will need to be expanded with logic to mock AWS Cognito authentication
+        response = self.app.post('/auth/login', data=dict(
+            username='testuser',
+            password='testpass'
+        ))
+        # Check for redirection (302) to the owner dashboard
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue('/owner/dashboard' in response.headers['Location'])
+
+if __name__ == '__main__':
+    unittest.main()
