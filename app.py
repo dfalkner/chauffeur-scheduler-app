de0b4d43604671e337c78005a8588c57cf98777d
Dane Falkner <dfalkner@surgeworks.com>
2023-11-15 14:10:23 -0700

    Added app.py with create_app function and hello route.

--- app.py
+++ app.py
@@ -2,14 +2,39 @@
 from flask import Flask
 import os
 
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=template_dir)
+
+def create_app(test_config=None):
+    # Create and configure the app
+    template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    app = Flask(__name__, template_folder=template_dir)
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+        # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+        # a simple page that says hello
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+        # Register blueprints and other app configurations here
+
+    # ...
+
+    return app
 
 
-@app.route('/')
-def index():
-    return "Welcome to the Chauffeur Scheduler App!"
-
-
+# Later on, you can create an app instance for running like this:
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
